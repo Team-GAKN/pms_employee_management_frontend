5300e039d7cc7a8ff33c48ec0967fee4abb0b47d
Kevin Wittmer <kevinwittmer@outlook.de>
2024-01-18 13:19:36 +0100

    #4 add employee-view components

---
+++ src/app/employee-view/employee-view.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-employee-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './employee-view.component.html',
+  styleUrl: './employee-view.component.css'
+})
+export class EmployeeViewComponent {
+
+}

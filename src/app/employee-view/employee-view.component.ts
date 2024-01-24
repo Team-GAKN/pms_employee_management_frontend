24facfd9ba4c16415502f97a8e2bb0781350a405
Kevinwi6 <kevin.w6@schule.bremen.de>
2024-01-24 10:20:53 +0100

    #4 add navbar with buttons

--- src/app/employee-view/employee-view.component.ts
+++ src/app/employee-view/employee-view.component.ts
@@ -1,13 +1,18 @@
+import { AppNavbarComponent } from '../app-navbar/app-navbar.component';
+import { AppButtonComponent} from "../app-button/app-button.component";
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ButtonVariant} from "../app-button/buttonVariants";
 
 @Component({
   selector: 'app-employee-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppNavbarComponent, AppButtonComponent],
   templateUrl: './employee-view.component.html',
   styleUrl: './employee-view.component.css'
 })
 export class EmployeeViewComponent {
 
+  protected readonly AppButtonComponent = AppButtonComponent;
+  protected readonly ButtonVariant = ButtonVariant;
 }

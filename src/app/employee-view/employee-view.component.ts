c948a86ec68ecbcbc24395ee04799bf2d6de767a
Kevinwi6 <kevin.w6@schule.bremen.de>
2024-01-25 13:09:17 +0100

    #4 add list of employees

--- src/app/employee-view/employee-view.component.ts
+++ src/app/employee-view/employee-view.component.ts
@@ -3,11 +3,12 @@ import { AppButtonComponent} from "../app-button/app-button.component";
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ButtonVariant} from "../app-button/buttonVariants";
+import {EmployeeListComponent} from "../employee-list/employee-list.component";
 
 @Component({
   selector: 'app-employee-view',
   standalone: true,
-  imports: [CommonModule, AppNavbarComponent, AppButtonComponent],
+  imports: [CommonModule, AppNavbarComponent, AppButtonComponent, EmployeeListComponent],
   templateUrl: './employee-view.component.html',
   styleUrl: './employee-view.component.css'
 })

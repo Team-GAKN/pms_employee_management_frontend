61454fc2ed390c217b35343f992b3218b775175b
Kevinwi6 <113449543+Kevinwi6@users.noreply.github.com>
2024-01-31 17:30:07 +0100

    #10 styling

--- src/app/employee-details-view/employee-details-view.component.ts
+++ src/app/employee-details-view/employee-details-view.component.ts
@@ -6,24 +6,24 @@ import {AppNavbarComponent} from "../app-navbar/app-navbar.component";
 import {QualificationLabelComponent} from "../qualification-label/qualification-label.component";
 import {ButtonVariant} from "../app-button/buttonVariants";
 import {Employee} from "../Employee";
-import {ActivatedRoute, Router} from "@angular/router";
 import {EmployeeSharingService} from "../service/employee-sharing.service";
 import {EmployeeCardComponent} from "../employee-card/employee-card.component";
+import {InputType} from "../app-input/inputType";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-employee-details-view',
   standalone: true,
-  imports: [CommonModule, AppButtonComponent, AppInputComponent, AppNavbarComponent, QualificationLabelComponent, EmployeeCardComponent],
+  imports: [CommonModule, AppButtonComponent, AppInputComponent, AppNavbarComponent, QualificationLabelComponent, EmployeeCardComponent, RouterLink],
   templateUrl: './employee-details-view.component.html',
   styleUrl: './employee-details-view.component.css'
 })
 export class EmployeeDetailsViewComponent {
   protected readonly ButtonVariant = ButtonVariant;
+  protected readonly InputType = InputType;
   employee: Employee | undefined;
 
-  constructor(private dataSharingService: EmployeeSharingService) {
-
-  }
+  constructor(private dataSharingService: EmployeeSharingService) {}
 
   ngOnInit() {
     this.employee = this.dataSharingService.getEmployee();

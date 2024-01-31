e6f9db8649bce46d6f15a6512fbb38deb7a9a676
Niels Hoppe <niels.hoppe@governikus.de>
2024-01-31 17:54:15 +0100

    #15 Backend API access
    finish data service

--- src/app/service/employee-data.service.ts
+++ src/app/service/employee-data.service.ts
@@ -11,15 +11,14 @@ import {TokenService} from "./token.service";
 export class EmployeeDataService {
 
   bearer: any = this.tokenService.getAccessToken();
-  allEmployees$: Observable<Employee[]> = this.setAllEmployees();
-  allQualifications$: Observable<Qualification[]> = this.setAllQualifications();
+  allEmployees$: Observable<Employee[]> = this.getAllEmployees();
+  allQualifications$: Observable<Qualification[]> = this.getAllQualifications();
 
   constructor(private http: HttpClient, private tokenService: TokenService) {}
 
-  setAllEmployees() {
-    console.log(this.bearer);
+  getAllEmployees() {
     this.allEmployees$ = of([]);
-    return this.http.get<Employee[]>('/backend', {
+    return this.http.get<Employee[]>('http://localhost:8089/employees', {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${this.bearer}`)
@@ -27,25 +26,56 @@ export class EmployeeDataService {
   }
 
   createEmployee(employee: Employee) {
-    this.http.post('/employees', employee, {
+    this.http.post(`http://localhost:8089/employees`, employee, {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${this.bearer}`)
-    });
-
+    })
+      .subscribe(
+        response => {
+          console.log('Erfolgreich erstellt:', response);
+        },
+        error => {
+          console.error('Fehler beim Erstellen:', error);
+        }
+      );
   }
 
-  updateEmployee(employee: Employee) {
-    this.http.put('/employee', employee, {
+  updateEmployee(id: number, employee: Employee) {
+    this.http.put(`http://localhost:8089/employees/${id}`, employee, {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${this.bearer}`)
-    });
+    })
+      .subscribe(
+        response => {
+          console.log('Erfolgreich erstellt:', response);
+        },
+        error => {
+          console.error('Fehler beim Erstellen:', error);
+        }
+      );
   }
 
-  setAllQualifications(): Observable<Qualification[]> {
+  deleteEmployee(id: number) {
+    return this.http.delete(`http://localhost:8089/employees/${id}`, {
+      headers: new HttpHeaders()
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${this.bearer}`)
+    })
+      .subscribe(
+        response => {
+          console.log('Erfolgreich erstellt:', response);
+        },
+        error => {
+          console.error('Fehler beim Erstellen:', error);
+        }
+      );
+  }
+
+  getAllQualifications(): Observable<Qualification[]> {
     this.allQualifications$ = of([]);
-    return this.http.get<Qualification[]>('/qualifications', {
+    return this.http.get<Qualification[]>('http://localhost:8089/qualifications', {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${this.bearer}`)
@@ -53,19 +83,35 @@ export class EmployeeDataService {
   }
 
   createQualification(qualification: Qualification) {
-    this.http.post('/qualifications', qualification, {
+    return this.http.post('http://localhost:8089/qualifications', qualification, {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${this.bearer}`)
-    });
+    })
+      .subscribe(
+        response => {
+          console.log('Erfolgreich erstellt:', response);
+        },
+        error => {
+          console.error('Fehler beim Erstellen:', error);
+        }
+      );
   }
 
-  updateQualification(qualification: Qualification) {
-    this.http.post('/qualifications', qualification, {
+  updateQualification(id: number, qualification: Qualification) {
+    this.http.put(`http://localhost:8089/qualifications/${id}`, qualification, {
       headers: new HttpHeaders()
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${this.bearer}`)
-    });
+    })
+      .subscribe(
+        response => {
+          console.log('Erfolgreich erstellt:', response);
+        },
+        error => {
+          console.error('Fehler beim Erstellen:', error);
+        }
+      );
   }
 
 }

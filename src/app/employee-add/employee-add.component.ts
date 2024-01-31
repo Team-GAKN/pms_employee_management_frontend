c7ed22fd3398213a1c82bdfd95bcc8c81fbee860
Kevinwi6 <113449543+Kevinwi6@users.noreply.github.com>
2024-01-31 17:23:52 +0100

    #12 you can create new employees

--- src/app/employee-add/employee-add.component.ts
+++ src/app/employee-add/employee-add.component.ts
@@ -5,15 +5,24 @@ import {AppButtonComponent} from "../app-button/app-button.component";
 import {ButtonVariant} from "../app-button/buttonVariants";
 import {AppInputComponent} from "../app-input/app-input.component";
 import {QualificationLabelComponent} from "../qualification-label/qualification-label.component";
+import {Employee} from "../Employee";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-employee-add',
   standalone: true,
-  imports: [CommonModule, AppNavbarComponent, AppButtonComponent, AppInputComponent, QualificationLabelComponent],
+  imports: [CommonModule, AppNavbarComponent, AppButtonComponent, AppInputComponent, QualificationLabelComponent, FormsModule],
   templateUrl: './employee-add.component.html',
   styleUrl: './employee-add.component.css'
 })
 export class EmployeeAddComponent {
-
   protected readonly ButtonVariant = ButtonVariant;
+
+  employee: Employee | undefined;
+
+  updateData(firstname: string, lastname: string, phone: string, postcode: string, city: string, street: string) {
+    this.employee = new Employee(undefined ,firstname, lastname, phone, postcode, city, street);
+  }
+
+
 }

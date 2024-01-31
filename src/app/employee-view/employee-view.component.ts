53d9a1112fc31b7fc4e423194c24a5f30d614773
Niels Hoppe <niels.hoppe@governikus.de>
2024-01-31 14:53:51 +0100

    #15 Backend API access
    swap fetchdata with employee-data.service call

--- src/app/employee-view/employee-view.component.ts
+++ src/app/employee-view/employee-view.component.ts
@@ -1,32 +1,23 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {Observable, of} from "rxjs";
-import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
 import {Employee} from "../Employee";
 import {EmployeeCardComponent} from "../employee-card/employee-card.component";
-import {TokenService} from "../service/token.service";
+import {EmployeeDataService} from "../service/employee-data.service";
 
 @Component({
   selector: 'app-employee-view',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, EmployeeCardComponent],
+  imports: [CommonModule, EmployeeCardComponent],
   templateUrl: './employee-view.component.html',
   styleUrl: './employee-view.component.css'
 })
 export class EmployeeViewComponent {
-  bearer = this.tokenService.getAccessToken();
-  employees$: Observable<Employee[]>;
 
-  constructor(private http: HttpClient, private tokenService: TokenService) {
-    this.employees$ = of([]);
-    this.fetchData();
+  employees$: Observable<Employee[]> = this.employeeDataService.allEmployees$;
+
+  constructor(private employeeDataService: EmployeeDataService) {
   }
 
-  fetchData() {
-    this.employees$ = this.http.get<Employee[]>('/backend', {
-      headers: new HttpHeaders()
-        .set('Content-Type', 'application/json')
-        .set('Authorization', `Bearer ${this.bearer}`)
-    });
-  }
+
 }
